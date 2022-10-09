ba4e7328c80771ad8322db04e6ef5ef06010c5be
Илья <@LAPTOP-J7PC779U>
2022-10-09 19:44:54 +1000

    Add project files.

---
+++ Functions/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int Add(int a, int b);
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
+int Sum(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "¬ведите два числа:"; cin >> a >> b;
+	int c = Add(a, b);
+	cout << a << "+" << b << "=" << c << endl;
+	cout << a << "-" << b << "=" << Sub(a, b) << endl;
+	cout << a << "*" << b << "=" << Mul(a, b) << endl;
+	cout << a << "/" << b << "=" << Div(a, b) << endl;
+	cout << "—умма элементов массива: " << Sum(arr, n) << endl;
+}
+int Add(int a, int b)
+{
+	//Addition
+	int c = a + b;
+	return c;
+}
+int Sub(int a, int b)
+{
+	//Subtraction
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	//Multiplication
+	return a * b;
+}
+double Div(int a, int b)
+{
+	//Division
+	return (double)a / b;
+}
+int Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
